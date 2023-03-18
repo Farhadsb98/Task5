e8f23ee30820d8b9a4e2207e15c810e6b371c52d
Farhadsb98 <124209727+Farhadsb98@users.noreply.github.com>
2023-03-18 11:47:18 +0400

    Proje dosyası ekle.

---
+++ Task5/Program.cs
@@ -0,0 +1,454 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task 1
+            //int cem =0;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    cem = cem + i;  
+            //}
+            //if (cem%7==0) 
+            //{
+            //    Console.WriteLine("Bolunur");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Bolunmur");
+            //}
+            #endregion
+
+            #region taks 2
+            //int cem = 0;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    cem =  i;
+
+            //    if (cem%7== 0 && cem%8==0) { 
+
+            //    Console.WriteLine(cem);
+
+            //    }
+
+            //}
+
+            #endregion
+
+            #region task3
+            //int reqem=0;
+            //int sum;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    sum = 0;
+
+            //    reqem = i;
+
+            //    //Console.WriteLine(i);
+
+            //    while (reqem > 0)
+            //    {
+            //        int faiz = reqem % 10;
+            //        reqem /= 10;
+            //        sum = sum + faiz;
+
+            //    }
+            //    if (sum%3==0)
+            //    {
+
+            //        Console.WriteLine( i);
+            //    }
+
+
+
+            //}
+
+            #endregion
+
+            #region task4
+            //int reqem = 0;
+            //int sum;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    sum = 0;
+
+            //    reqem = i;
+
+
+
+            //    while (reqem > 0)
+            //    {
+            //        int faiz = reqem % 10;
+            //        reqem /= 10;
+            //        sum = sum + faiz;
+
+            //    }
+            //    if (sum % 3 == 0)
+            //    {
+            //        if (i % 10 == 3)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+
+            //    }
+
+
+
+            //}
+            #endregion
+
+            #region task5
+            //int reqem = 0;
+            //int sum;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    sum = 0;
+
+            //    reqem = i;
+
+
+
+            //    while (reqem > 0)
+            //    {
+            //        int faiz = reqem % 10;
+            //        reqem /= 10;
+            //        sum = sum + faiz;
+
+            //    }
+            //    if (sum % 5 == 0)
+            //    {
+            //        if (i % 10 == 5)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+
+            //    }
+
+
+
+            //}
+            #endregion
+
+            #region task6
+            //int reqem = 0;
+            //int sum;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    sum = 0;
+
+            //    reqem = i;
+            //    if (reqem % 2 == 0)
+            //    {
+            //        int reqem1 = i;
+            //        int sum2 = 0;
+            //        while (reqem1 > 0)
+            //        {
+            //            int faiz = reqem1 % 10;
+            //            reqem1 /= 10;
+            //            sum2 = sum2 + faiz;
+            //        }
+            //        if (sum2 % 2 == 1)
+            //        {
+            //            int say = (int)Math.Log10(i);
+            //            int ilkreqem = i / (int)Math.Pow(10, say);
+            //            if (ilkreqem % 2 == 1)
+            //            {
+            //                Console.WriteLine(i);
+            //            }
+
+
+            //        }
+            //    }
+
+            //}
+            #endregion
+
+
+            #region task7
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    int reqem = i;
+            //    bool Three = false;
+            //    while (reqem > 0)
+            //    {
+            //        int faiz = reqem % 10;
+            //        reqem /= 10;
+
+            //        if (faiz == 3)
+            //        {
+            //            Three = true;
+            //            break;
+            //        }
+            //    }
+
+            //    if (!Three)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+
+
+            #endregion
+
+            #region task8
+            //int enboyuk = 0;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    int reqem = i;
+            //    int reqem1= i;
+            //    bool Three = false;
+            //    while (reqem > 0)
+            //    {
+            //        int faiz = reqem % 10;
+            //        reqem /= 10;
+
+            //        if (faiz == 3)
+            //        {
+            //            Three = true;
+            //            break;
+            //        }
+            //    }
+
+            //    if (!Three)
+            //    {
+            //        int sum = 0;
+            //        while ( reqem1> 0) 
+            //        {
+            //            int faiz1= reqem1% 10;
+            //            reqem1 /= 10;
+            //            sum =sum + faiz1;
+            //        }
+            //        if (sum==3)
+            //        {
+
+            //            if (i > enboyuk)
+            //            {
+            //                enboyuk= i;
+
+            //            }
+
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(enboyuk);
+            #endregion
+
+            #region task9
+            //int reqem;
+            //int say11 = 0;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    reqem = i;
+            //    if (reqem%11==0)
+            //    {
+            //        int sum = 0;
+            //        while (reqem>0)
+            //        {
+            //            int faiz = reqem % 10;
+            //            reqem /= 10;
+            //            sum =sum + faiz;
+            //        }
+            //        if (sum>11)
+            //        {
+            //            say11++;
+            //            if (say11==11)
+            //            {
+            //                Console.WriteLine(i);
+            //                break;
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region task 10
+
+            //int reqem, enboyuk = 0;
+            //int sum;
+            //for (int i = 300; i < 1001; i++)
+            //{
+            //    sum = 0;
+
+            //    reqem = i;
+
+            //    if (reqem % 2 == 0)
+            //    {
+            //        while (reqem > 0)
+            //        {
+            //            int faiz = reqem % 10;
+            //            reqem /= 10;
+            //            sum += faiz;
+
+
+            //        }
+            //        if (sum < 7 && sum > 5)
+            //        {
+
+            //            if (i > enboyuk)
+            //            {
+            //                enboyuk=i;
+
+            //            }
+            //        }
+            //    }
+
+
+
+
+
+
+            //}
+            //Console.WriteLine(enboyuk);
+            #endregion
+
+            #region task11
+            //int reqem;
+
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    int sum = 0;
+            //    reqem = i;
+            //    int reqem1 = i;
+            //    int sum1 = 0;
+            //    int reqem2 = i;
+            //    int sum2 = 0;
+            //    bool three = false;
+            //    while (reqem > 0)
+            //    {
+            //        int faiz = reqem % 10;
+            //        reqem /= 10;
+            //        sum = sum * 10 + faiz;
+            //    }
+            //    if (i == sum)
+            //    {
+            //        while (reqem1 > 0)
+            //        {
+            //            int faiz1 = reqem1 % 10;
+            //            reqem1 /= 10;
+            //            if (faiz1 == 3)
+            //            {
+            //                three = true;
+            //                break;
+            //            }
+            //        }
+
+            //        if (!three)
+            //        {
+            //            while (reqem2 > 0)
+            //            {
+            //                int faiz2 = reqem2 % 10;
+            //                reqem2 /= 10;
+            //                sum2 = sum2 + faiz2;
+            //            }
+            //        }
+            //        if (sum > 10)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+
+            //}
+            #endregion
+
+            #region taks 12
+            for (int i = 10; i <= 100000; i++)
+            {
+                int sum = 0;
+                bool veziyyet=false;
+                int reqem = i;
+                int reqem1 = i;
+                int oldFaiz = 0;
+              
+                while (reqem > 0)
+                {
+                    int faiz = reqem % 10;
+                    reqem /= 10;
+                    
+
+
+                    sum += faiz;
+                  if (faiz != faiz_2) 
+                    {
+                        veziyyet= true;
+                        break;
+                    }
+
+
+                }
+                if (!veziyyet)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            #endregion
+
+            #region taks 13
+            //for (int i = 0; i < 100001; i++)
+            //{
+            //    int reqem = i;
+            //    int reqem1= i;
+            //    int fa = 0;
+
+            //    if (reqem%2==1)
+            //    {
+
+            //        int sum = 0;
+            //        while (reqem>0)
+            //        {
+            //            int faiz = reqem % 10;
+            //            reqem/=10;
+            //            sum=sum*10+faiz;
+
+            //        }
+            //        if (i==sum)
+            //        {
+            //            while (reqem1>0)
+            //            {
+            //                int faiz1 = reqem1 % 10; 
+            //                reqem1/=10;
+            //               fa=faiz1;
+            //            }
+            //            if (fa == 5)
+            //            {
+            //                Console.WriteLine(i);
+            //            }
+
+
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region task 14
+            //for (int i = 0; i < 10001; i++)
+            //{
+            //    int reqem = i;
+            //    for (int j = 2; j < 10001; j++)
+            //    {
+            //        if (i%j==0)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+
+
+            //    while (reqem > 0)
+            //    {
+            //        int faiz = reqem % 10;
+            //        reqem /= 10;
+            //    }
+            //}
+            #endregion
+
+
+
+        }
+    
+    }
+}
